56290c016a7c23431a2b7dad93717f880f73b659
Daniel Karpierz <dankarp1999@gmail.com>
2022-08-12 14:56:20 -0400

    Add renderGameBoard function to display moves

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+//module for keeping track of the board positions
 const Gameboard = (function () {
-	gameboard = [];
+	const boardPositions = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O', 'X'];
+
+	return { boardPositions };
 })();
 
+//factory function for creating players
 const Player = (name) => {
 	const sayName = () => console.log(`my name is ${name}`);
+
 	return { sayName };
 };
+
+const renderGameBoard = (function () {
+	let cell = document.querySelectorAll('.cell');
+	const update = () => {
+		for (let i = 0; i < 9; i++) {
+			cell[i].textContent = Gameboard.boardPositions[i];
+		}
+		console.log(`${Gameboard.boardPositions}`);
+	};
+	return { update };
+})();
+
+renderGameBoard.update();
