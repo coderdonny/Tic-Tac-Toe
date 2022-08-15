fc92cc342e19d86e95eff026e1316a9f7e7ebfd4
Daniel Karpierz <dankarp1999@gmail.com>
2022-08-15 12:17:51 -0400

    Add initializeGame function to allow player one to choose X or O

--- script.js
+++ script.js
@@ -1,19 +1,78 @@
+const playerX = document.querySelector('.playerX');
+const playerO = document.querySelector('.playerO');
+
+//player One chooses X or O
+const initializeGame = (function () {
+	playerX.dataset.pick = '';
+	playerO.dataset.pick = '';
+
+	const choice = (e) => {
+		if (!(playerX.dataset.pick === '' && playerO.dataset.pick === '')) {
+			return;
+		}
+		if (
+			e.target.classList.contains('playerX') &&
+			playerX.dataset.pick === ''
+		) {
+			Player.playerOne = 'X';
+			playerX.dataset.pick = 'playerOne';
+
+			Player.playerTwo = 'O';
+			playerO.dataset.pick = 'playerTwo';
+
+			console.log('Player One chose X');
+		} else {
+			Player.playerOne = 'O';
+			playerO.dataset.pick = 'playerOne';
+
+			Player.playerTwo = 'X';
+			playerX.dataset.pick = 'playerTwo';
+
+			console.log('Player One chose O');
+		}
+	};
+
+	const isGameInitialized = () => {
+		if (!(playerX.dataset.pick === '' && playerO.dataset.pick === '')) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	playerX.addEventListener('click', choice);
+	playerO.addEventListener('click', choice);
+
+	return { isGameInitialized };
+})();
+
 //module for keeping track of the board positions
 const Gameboard = (function () {
-	const boardPositions = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O', 'X'];
+	// const boardPositions = ['X', 'X', 'O', 'X', 'O', 'O', 'X', 'O', 'X'];
+	const boardPositions = [
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+	];
 
 	return { boardPositions };
 })();
 
-//factory function for creating players
-const Player = (name) => {
-	const sayName = () => console.log(`my name is ${name}`);
-
-	return { sayName };
+//function for keeping track of players
+const Player = () => {
+	return { playerOne, playerTwo };
 };
 
+let cell = document.querySelectorAll('.cell');
+
+//updates the game UI when player makes a move
 const renderGameBoard = (function () {
-	let cell = document.querySelectorAll('.cell');
 	const update = () => {
 		for (let i = 0; i < 9; i++) {
 			cell[i].textContent = Gameboard.boardPositions[i];
@@ -21,6 +80,30 @@ const renderGameBoard = (function () {
 		console.log(`${Gameboard.boardPositions}`);
 	};
 	return { update };
+})();
+
+//controls who's turn it is and sends the move to Gameboard
+const gameControl = (function () {
+	const playerTurn = () => {};
+
+	const makeMove = (e) => {
+		if (initializeGame.isGameInitialized()) {
+			const target = e.target;
+			target.textContent = 'X';
+			Gameboard.boardPositions.splice(
+				target.dataset.cellNumber,
+				1,
+				Player.playerOne
+			);
+		} else {
+			console.log('Game has not been initialized');
+			return;
+		}
+	};
+
+	for (let i = 0; i < 9; i++) {
+		cell[i].addEventListener('click', makeMove);
+	}
 })();
 
 renderGameBoard.update();
